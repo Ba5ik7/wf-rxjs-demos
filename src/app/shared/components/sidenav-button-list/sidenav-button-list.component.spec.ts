a711697e8b77a071f31c64879ca181929ed0b057
Wesley DuSell <dusellfam@gmail.com>
2022-12-27 18:32:28 -0500

    upgrading to angular 11

--- src/app/shared/components/sidenav-button-list/sidenav-button-list.component.spec.ts
+++ src/app/shared/components/sidenav-button-list/sidenav-button-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SidenavButtonListComponent } from './sidenav-button-list.component';
 
@@ -6,7 +6,7 @@ describe('SidenavButtonListComponent', () => {
   let component: SidenavButtonListComponent;
   let fixture: ComponentFixture<SidenavButtonListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SidenavButtonListComponent ]
     })
